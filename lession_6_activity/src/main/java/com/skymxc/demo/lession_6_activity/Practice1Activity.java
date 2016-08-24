39fe3d2cab3989bec2c96347733b7ba38efedf4c
mxc <164443895@qq.com>
2016-08-25 00:28:14 +0800

    Update Practice1Activity.java

--- lession_6_activity/src/main/java/com/skymxc/demo/lession_6_activity/Practice1Activity.java
+++ lession_6_activity/src/main/java/com/skymxc/demo/lession_6_activity/Practice1Activity.java
@@ -12,26 +12,27 @@ import java.util.ArrayList;
 
 public class Practice1Activity extends AppCompatActivity {
 
+    //下拉框
     private Spinner spinner ;
-
+    //绑定下来框使用的数据
     private ArrayList<String> entries ;
-
+    //移除按钮
     private Button btremove ;
-
+    //增加按钮
     private Button btAdd ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice1);
-
+        //初始化操作
         spinner = (Spinner) findViewById(R.id.spinner);
         btremove = (Button) findViewById(R.id.remove);
         btAdd = (Button) findViewById(R.id.add);
 
         entries = new ArrayList<String>();
         entries.add("java");
-
+        //绑定数据源适配器
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,entries);
         spinner.setAdapter(adapter);
     }
@@ -43,13 +44,13 @@ public class Practice1Activity extends AppCompatActivity {
     public void click(View v ){
         Intent intent =null;
         switch (v.getId()){
-            case R.id.add:
+            case R.id.add:  //增加
                  intent = new Intent(this,Practice1InputActivity.class);
                 intent.putExtra("opr","add");
                 intent.putStringArrayListExtra("entries",entries);
                 startActivityForResult(intent,1);
                 break;
-            case R.id.remove:
+            case R.id.remove:   //移除
                  intent = new Intent(this,Practice1InputActivity.class);
                 intent.putExtra("opr","remove");
                 intent.putStringArrayListExtra("entries",entries);
@@ -61,17 +62,18 @@ public class Practice1Activity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (data!=null){
+        if (data!=null){ 
             entries = data.getStringArrayListExtra("entries");
             ArrayAdapter adapter= new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,entries);
             spinner.setAdapter(adapter);
 
+            //如果下来匡的数据超过5个就禁用增加按钮
             if (entries.size()>=5){
                 btAdd.setEnabled(false);
             }else{
                 btAdd.setEnabled(true);
             }
-
+            //如果下拉框没有数据 就禁用 移除按钮
             if (entries.size()==0){
                 btremove.setEnabled(false);
             }else {
