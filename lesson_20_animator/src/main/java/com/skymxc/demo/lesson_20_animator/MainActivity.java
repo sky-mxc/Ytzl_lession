b54f7c44b3fa18dd7c32e35d51cc7a015bc18b05
mxc <164443895@qq.com>
2016-09-19 17:12:53 +0800

    制作SD卡文件浏览器

--- lesson_20_animator/src/main/java/com/skymxc/demo/lesson_20_animator/MainActivity.java
+++ lesson_20_animator/src/main/java/com/skymxc/demo/lesson_20_animator/MainActivity.java
@@ -1,13 +1,143 @@
 package com.skymxc.demo.lesson_20_animator;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String  TAG ="MainActivity";
+
+    private Button btValuesAnimator;
+    private ValueAnimator valueAnimator;
+    private Button btObjectAnimator;
+    private ObjectAnimator objectAnimator;
+    private Button btSet;
+    private CheckBox cbTegether;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+
+        //============AnimatorSet
+    //    initAnimatorSet();
+
+        //===========ObjectAnimator
+        initObjectAnimator();
+
+        //===========valueAnimator
+        initValueAnimator();
+
+
+    }
+
+    private void initView() {
+        btValuesAnimator = (Button) findViewById(R.id.value_animator);
+        btObjectAnimator = (Button) findViewById(R.id.object_animator);
+        btSet =  (Button)findViewById(R.id.set_animator);
+        cbTegether = (CheckBox) findViewById(R.id.tegether);
+    }
+
+    private void initAnimatorSet() {
+        AnimatorSet set = new AnimatorSet();
+        set.setDuration(6000);
+
+//        PropertyValuesHolder translationX = PropertyValuesHolder.ofFloat("translationX",0,300);
+//        PropertyValuesHolder translationY = PropertyValuesHolder.ofFloat("translationY",0,200);
+//        ObjectAnimator x = ObjectAnimator.ofPropertyValuesHolder(btSet,translationX);
+//        ObjectAnimator y = ObjectAnimator.ofPropertyValuesHolder(btSet,translationY);
+
+        ObjectAnimator x = ObjectAnimator.ofFloat(btSet,"translationX",0,300);
+        ObjectAnimator y = ObjectAnimator.ofFloat(btSet,"translationY",0,200);
+        if (cbTegether.isChecked()){
+            set.playTogether(x,y);         //同时播放
+        }else{
+            set.playSequentially(x,y);     //序列播放
+        }
+    set.start();
+    }
+
+    private void initObjectAnimator() {
+       // objectAnimator = (ObjectAnimator) AnimatorInflater.loadAnimator(this, R.animator.animator_object);
+        /**
+         * 设置颜色
+         */
+//        objectAnimator = ObjectAnimator.ofInt(btObjectAnimator,"textColor", Color.YELLOW,Color.RED);
+//        objectAnimator.setDuration(6000);
+
+        /**
+         * 移动
+         */
+        PropertyValuesHolder translationX = PropertyValuesHolder.ofFloat("translationX",0,300);
+        PropertyValuesHolder translationY = PropertyValuesHolder.ofFloat("translationY",0,200);
+
+        objectAnimator = ObjectAnimator.ofPropertyValuesHolder(btObjectAnimator,translationX,translationY);
+        objectAnimator.setDuration(6000);
+    }
+
+    private void initValueAnimator() {
+        // valueAnimator = (ValueAnimator) AnimatorInflater.loadAnimator(this,R.animator.animator_values);
+        // valueAnimator = ValueAnimator.ofFloat(0,1);
+        // valueAnimator = ValueAnimator.ofInt(Color.RED,Color.BLUE);
+
+        PropertyValuesHolder alphaHolder = PropertyValuesHolder.ofFloat("alpha",0.3f,0.7f);
+        PropertyValuesHolder widthHolder = PropertyValuesHolder.ofInt("width",0,300);
+
+        valueAnimator = ValueAnimator.ofPropertyValuesHolder(alphaHolder,widthHolder);
+        valueAnimator.setDuration(10000);
+        //   valueAnimator.setInterpolator( new CycleInterpolator(1));
+        valueAnimator.addUpdateListener(updateLis);
+    }
+
+    private ValueAnimator.AnimatorUpdateListener updateLis = new ValueAnimator.AnimatorUpdateListener() {
+        @Override
+        public void onAnimationUpdate(ValueAnimator valueAnimator) {
+
+
+          // int value = (int) valueAnimator.getAnimatedValue();
+//            btValuesAnimator.setTranslationX(value);
+
+         //   float value = (float) valueAnimator.getAnimatedValue();
+         //   btValuesAnimator.setAlpha(value);
+        //    btValuesAnimator.setTextColor(value);
+
+
+            float value= (float) valueAnimator.getAnimatedValue("alpha");
+            int width= (int) valueAnimator.getAnimatedValue("width");
+
+            Log.e(TAG,"========value========"+value);
+            Log.e(TAG,"========width========"+width);
+
+            btValuesAnimator.setAlpha(value);
+            btValuesAnimator.setRight(width);//
+        }
+    };
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.value_animator:
+                valueAnimator.start();
+                break;
+            case R.id.object_animator:
+                objectAnimator.setTarget(btObjectAnimator);
+                objectAnimator.start();
+                break;
+            case R.id.set_animator:
+
+                initAnimatorSet();
+
+                break;
+        }
     }
 }
--- lesson_20_animator/src/main/java/com/skymxc/demo/lesson_20_animator/practice/FileActivity.java
+++ lesson_20_animator/src/main/java/com/skymxc/demo/lesson_20_animator/practice/FileActivity.java
@@ -1,7 +1,153 @@
 package com.skymxc.demo.lesson_20_animator.practice;
 
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.skymxc.demo.lesson_20_animator.R;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by sky-mxc
  */
-public class FileActivity {
+public class FileActivity extends AppCompatActivity {
+
+    private static final String TAG ="FileActivity";
+
+    private ListView fileLv;
+    private TextView tvTitle;
+    private FileAdapter adapter;
+    private List<File> files;
+    private File currentFile;
+    private  TextView tvBack;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice_main);
+        initView();
+    }
+
+
+    private AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            if (l<0){
+                //back
+                backUp();
+                return;
+            }
+            currentFile = adapter.getItem((int)l);
+            if (currentFile.isFile()){
+                Toast.makeText(FileActivity.this,currentFile.getName(),Toast.LENGTH_SHORT).show();
+            }else{
+               log("subFiles:"+currentFile.listFiles().length);
+                if (fileLv.getHeaderViewsCount()<1){
+                    fileLv.setAdapter(null);
+                     tvBack = new TextView(FileActivity.this);
+                    tvBack.setText("返回上一级");
+                    AbsListView.LayoutParams params = new AbsListView.LayoutParams(500,50);
+                    params.height=50;
+                    tvBack.setLayoutParams(params);
+                    tvBack.setPadding(10,10,10,10);
+                    tvBack.setGravity(Gravity.CENTER_VERTICAL);
+                    fileLv.addHeaderView(tvBack);
+                }
+                files = Arrays.asList(currentFile.listFiles());
+                fileLv.setAdapter(adapter);
+                tvTitle.setText(currentFile.getName());
+            }
+        }
+    };
+
+    /**
+     * 回到上一级
+     */
+    private void backUp() {
+        if (Environment.getExternalStorageDirectory().getPath().equals(currentFile.getParentFile().getPath())){
+            //上一级是根目录
+            fileLv.setAdapter(null);
+            fileLv.removeHeaderView(tvBack);
+            fileLv.setAdapter(adapter);
+            tvTitle.setText("SDCard");
+        }else{
+            tvTitle.setText(currentFile.getParentFile().getName());
+        }
+        //不是根目录
+        files = Arrays.asList(currentFile.getParentFile().listFiles());
+        currentFile= currentFile.getParentFile();
+        adapter.notifyDataSetChanged();
+    }
+
+    private void initView() {
+        fileLv = (ListView) findViewById(R.id.file_lv);
+        tvTitle = (TextView) findViewById(R.id.title);
+        currentFile= Environment.getExternalStorageDirectory();
+        files = Arrays.asList(currentFile.listFiles());
+        adapter = new FileAdapter();
+        fileLv.setAdapter(adapter);
+        fileLv.setOnItemClickListener(onItemClickListener);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if ( Environment.getExternalStorageDirectory().getPath().equals(currentFile.getPath())){
+            super.onBackPressed();
+        }else{
+            backUp();
+        }
+
+
+    }
+
+    public static void log(Object str){
+        Log.e(TAG,"====="+str+"=====");
+    }
+
+    class  FileAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return files.size();
+        }
+
+        @Override
+        public File getItem(int i) {
+            return files.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            TextView tv;
+            if (view ==null){
+                view= getLayoutInflater().inflate(android.R.layout.simple_list_item_1,null);
+                 tv = (TextView) view;
+
+            }else{
+                tv = (TextView) view;
+            }
+            File file = getItem(i);
+            tv.setText(file.getName());
+
+            return tv;
+        }
+    }
 }
